afd2f7d8b88515876ea29e7db9c26d5d9b60a285
Leonardo Delgado Medrano <leonardo.delgado.medrano@gmail.com>
2024-11-18 04:03:38 -0400

    Login

--- PortalAdmin/src/services/login.js
+++ PortalAdmin/src/services/login.js
@@ -6,27 +6,19 @@ export default class Login {
 
     static async handleLoginResponse() {
     try {
-        // Llama al endpoint que devuelve los datos del usuario después de iniciar sesión
-        const response = await fetch("http://localhost:8080/login/success"); // Ajusta esta URL si es necesario
-
+        const response = await fetch("http://localhost:8080/api/v1/personas"); // Ajusta esta URL si es necesario
+        console.log("Hasta el response");
         if (response.ok) {
-            // Convierte la respuesta a JSON
             const userData = await response.json();
-
-            // Guarda los datos en localStorage
             localStorage.setItem("user", JSON.stringify(userData));
 
             console.log("Inicio de sesión exitoso:", userData);
-
-            // Mensaje de bienvenida al usuario
             alert(`Bienvenido, ${userData.name}`);
         } else {
-            // Maneja errores en caso de respuesta no exitosa
             console.error("Error al obtener datos del usuario:", response.statusText);
             alert("Error al iniciar sesión. Por favor, intenta de nuevo.");
         }
     } catch (error) {
-        // Captura errores en la conexión o problemas con el fetch
         console.error("Error al manejar la respuesta del inicio de sesión:", error);
         alert("Ocurrió un error inesperado. Por favor, verifica tu conexión.");
     }
